25ea62d1e9a50da97eaa69f3c7c4a091892f7084
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-02 11:05:08 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+  # gedi-patch
+
+  Given changes calculated between two JS objects (using
+  [xdiff](https://github.com/dominictarr/xdiff)), update a gedi model.
+
+  ## Example Usage
+
+  To be completed.
+
+**/
+module.exports = function(model) {
+  return function(diff) {
+  };
+};
---
+++ test/from-empty.js
@@ -0,0 +1,22 @@
+var Gedi = require('gedi');
+var test = require('tape');
+var x = require('xdiff');
+var data;
+var patch;
+
+test('can create a new gedi instance', function(t) {
+  t.plan(1);
+  t.ok(data = new Gedi());
+});
+
+test('can create a patch function using the data', function(t) {
+  t.plan(1);
+  patch = require('../')(data);
+  t.equal(typeof patch, 'function');
+});
+
+test('can apply required patches for a single key change', function(t) {
+  t.plan(1);
+  x.diff({}, { name: 'Fred' }).forEach(patch);
+  t.equal(data.get('[/name]'), 'Fred');
+});
