3a0677868ecbe4bf511f8c3e2f5ee023cd8e5e6b
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-02 11:29:10 +1000

    Additional tests

--- test/from-empty.js
+++ test/from-empty.js
@@ -33,3 +33,83 @@ test('can apply required patches for multiple key changes', function(t) {
   t.equal(data.get('[/name]'), 'Fred');
   t.equal(data.get('[/age]'), 52);
 });
+
+test('can apply multiple deletes', function(t) {
+  t.plan(1);
+  x.diff(data.get(), {}).forEach(patch);
+  t.deepEqual(data.get(), {}, 'reset data');
+});
+
+test('can apply nested object patches for new compound objects', function(t) {
+  var newData = {
+    fred: {
+      name: 'Fred Smith',
+      age: 52
+    },
+
+    bob: {
+      name: 'Bob Clownpants',
+      age: 25
+    }
+  };
+
+  t.plan(1);
+  x.diff(data.get(), newData).forEach(patch);
+  t.deepEqual(data.get(), newData);
+});
+
+test('can add a nested value', function(t) {
+  var newData = {
+    fred: {
+      name: 'Fred Smith',
+      location: 'Tahiti',
+      age: 52
+    },
+
+    bob: {
+      name: 'Bob Clownpants',
+      age: 25
+    }
+  };
+
+  t.plan(1);
+  x.diff(data.get(), newData).forEach(patch);
+  t.deepEqual(data.get(), newData);
+});
+
+test('can update nested value', function(t) {
+  var newData = {
+    fred: {
+      name: 'Fred Smith',
+      location: 'Tahiti',
+      age: 53
+    },
+
+    bob: {
+      name: 'Bob Clownpants',
+      age: 25
+    }
+  };
+
+  t.plan(1);
+  x.diff(data.get(), newData).forEach(patch);
+  t.deepEqual(data.get(), newData);
+});
+
+test('can remove a nested value', function(t) {
+  var newData = {
+    fred: {
+      name: 'Fred Smith',
+      age: 53
+    },
+
+    bob: {
+      name: 'Bob Clownpants',
+      age: 25
+    }
+  };
+
+  t.plan(1);
+  x.diff(data.get(), newData).forEach(patch);
+  t.deepEqual(data.get(), newData);
+});
